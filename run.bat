59db40c5786e129c3c8df13aaf247639b2a1d7b6
Timothy <timo.dev96@gmail.com>
2026-07-19 15:14:15 +0300

    Edited run.bat

--- run.bat
+++ run.bat
@@ -6,6 +6,11 @@ if exist app-unsigned.apk del /q app-unsigned.apk
 if exist app-aligned.apk del /q app-aligned.apk
 if exist app-signed.apk del /q app-signed.apk
 if exist classes.dex del /q classes.dex
+if not exist debug.keystore (
+    echo debug.keystore not found. Generating a new one...
+    call "B:\Java JDK 21\bin\keytool.exe" -genkey -v -keystore debug.keystore -storepass android -alias androiddebugkey -keypass android -keyalg RSA -keysize 2048 -validity 10000 -dname "CN=Android Debug,O=Android,C=US"
+    if %errorlevel% neq 0 (echo Keystore Generation Failed! && exit /b)
+)
 
 call B:\Hsoub\Kotlin\AndroidSDK\build-tools\33.0.2\aapt.exe package -f -m -J . -M AndroidManifest.xml -S res -I B:\Hsoub\Kotlin\AndroidSDK\platforms\android-33\android.jar
 if %errorlevel% neq 0 (echo Resource Generation Failed! && exit /b)
